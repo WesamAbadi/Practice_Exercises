41a178a24bf6500ceffe817c3b14a377ab6ff7a6
Wesam Abadi <wisamabady7@gmail.com>
2021-10-25 17:47:22 +0200

    Fibonacci sequence calculator

---
+++ Practice_1/Fibonacci/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fNumber;
+            int input;
+            Console.WriteLine("WRITE THE NUMBER ");
+            int.TryParse(Console.ReadLine(), out input);
+
+            if (input > 0)
+                Calculator(input);
+            else
+                Console.WriteLine("ERROR");
+
+            Console.ReadKey();
+        }
+
+        private static void Calculator(int input)
+        {
+            int start = 0;
+            int result = 0;
+
+            for (int i = 1; i <= input; i++)
+            {
+                result = i + start;
+                start++;
+                Console.Write(result + ", ");
+            }
+            //Console.WriteLine(result);
+        }
+    }
+}
