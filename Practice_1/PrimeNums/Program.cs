df9fd50aa3ed0aa93fe79b358ecac41049fb9cd7
Wesam Abadi <wisamabady7@gmail.com>
2021-10-25 17:44:58 +0200

    Prime Numbers

---
+++ Practice_1/PrimeNums/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 0;
+            int end = 0;
+            bool result;
+            Console.WriteLine("WRITE THE START : ");
+            int.TryParse(Console.ReadLine(), out start);
+
+            Console.WriteLine("WRITE THE END : ");
+            int.TryParse(Console.ReadLine(), out end);
+
+            if (start > 0 && end > 0)
+            {
+                for (; start <= end; start++)
+                {
+                    result = PrimeNum(start);
+                    // Console.WriteLine("THE resylt is " + result);
+                    if (result)
+                        Console.WriteLine($"{start} is Prime");
+                    else
+                        Console.WriteLine($"{start} is NOT Prime");
+                }
+
+            }
+            Console.ReadKey();
+        }
+
+        private static bool PrimeNum(int start)
+        {
+
+            bool result = true;
+
+
+            for (int j = 2; j <= Math.Sqrt(start); j++)
+            {
+                if (start % j == 0)
+                {
+                    result = false;
+                    //Console.WriteLine($"{start} is not Prime");
+
+                }
+                else if (start % j != 0)
+                {
+                    result = true;
+                    //Console.WriteLine($"{start} is Prime");
+                }
+            }
+
+
+            return result;
+        }
+    }
+}
--- Practice_1/PrimeNums/Properties/AssemblyInfo.cs
+++ Practice_1/PrimeNums/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Factorial")]
+[assembly: AssemblyTitle("PrimeNums")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Factorial")]
+[assembly: AssemblyProduct("PrimeNums")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("573c8d9b-e1a2-4f63-bdce-3ff7076971da")]
+[assembly: Guid("77e2d26c-59e5-46cd-9c75-2b8cba330756")]
 
 // Version information for an assembly consists of the following four values:
 //
