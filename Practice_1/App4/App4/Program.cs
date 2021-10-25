43db8f7f46c3ac0e5436c153ec515a3820d16176
Wesam Abadi <wisamabady7@gmail.com>
2021-10-25 17:22:50 +0200

    LOCAL

--- Practice_1/App4/App4/Program.cs
+++ Practice_1/App4/App4/Program.cs
@@ -10,6 +10,43 @@ namespace App4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What's your grade ? ");
+            int input = int.Parse(Console.ReadLine());
+            int grade = 0;
+
+
+            ranger(input, grade);
+            Console.ReadKey();
+
         }
+
+        private static void ranger(int input, int grade)
+        {
+            switch (input)
+            {
+                case int n when n > 0 && n <= 50:
+                    grade = 1;
+                    break;
+
+                case int n when n > 51 && n <= 65:
+                    grade = 2;
+                    break;
+
+                case int n when n > 66 && n <= 75:
+                    grade = 3;
+                    break;
+
+                case int n when n > 76 && n <= 85:
+                    grade = 4;
+                    break;
+
+                case int n when n > 86 && n <= 100:
+                    grade = 5;
+                    break;
+            }
+            Console.WriteLine($"THE GRADE IS {grade}!");
+        }
+
     }
 }
+
