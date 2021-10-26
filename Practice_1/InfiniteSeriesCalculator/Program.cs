e335858e50345454dec3ba275311d150c27576aa
Wesam Abadi <wisamabady7@gmail.com>
2021-10-26 11:33:30 +0200

    Created the InfiniteSeriesCalculator project

---
+++ Practice_1/InfiniteSeriesCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteSeriesCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        static List<int> sumList = new List<int>();
+
+
+
+    }
+}
