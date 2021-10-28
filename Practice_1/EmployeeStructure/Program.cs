e8bc8c07a0ec29b0d87e20b566c99a7fe0859ace
Wesam Abadi <wisamabady7@gmail.com>
2021-10-28 13:11:07 +0200

    Finalized

--- Practice_1/EmployeeStructure/Program.cs
+++ Practice_1/EmployeeStructure/Program.cs
@@ -7,25 +7,26 @@ using System.Threading.Tasks;
 namespace EmployeeStructure
 {
     class Program
-    { 
+    {
         struct Employee
         {
 
             public string empName;
-            public int hoursNum, hourlyRate, wage ;
-            
+            public int hoursNum, hourlyRate;
+            private static int wage;
 
 
-            public Employee(string name, int hours, int rate, int wage)
+
+            public Employee(string name, int hours, int rate)
             {
                 List<string> myEmployees = new List<string>();
-                
+
                 this.empName = name;
                 this.hoursNum = hours;
                 this.hourlyRate = rate;
-                this.wage = wage;
+
                 Claculator(hours, rate);
-                Result();
+
                 myEmployees.Add(Resultttt());
             }
 
@@ -35,37 +36,55 @@ namespace EmployeeStructure
             }
 
             public int Claculator(int hours, int rate) => hours * rate;
-            public void Result() => Console.WriteLine($"NAME : {this.empName}\n HOURS NUMBER :{+this.hoursNum}\n HOURLY RATE : {+this.hourlyRate}\n HIS WAGE : {+ Claculator(hoursNum, hourlyRate)}");
+            public override string ToString() => ($"NAME : {this.empName}\n HOURS NUMBER :{+this.hoursNum}\n HOURLY RATE : {+this.hourlyRate}\n HIS WAGE : {+Claculator(hoursNum, hourlyRate)}");
 
+
+
+            public static int Wage
+            {
+                get { return wage; }
+                set { wage = value; }
+            }
         }
 
         static void Main(string[] args)
         {
-            //List<Employee> myEmployees = new List<Employee>();
+
+            List<Employee> myEmployees = new List<Employee>();
             char answer = 'Y';
+            int numOf = 0;
             while (answer == 'Y')
             {
 
-            Console.WriteLine("NAME : ");
-            string empName = Console.ReadLine();
-            Console.WriteLine("NUMBER OF HOURS :");
-            int hours = int.Parse(Console.ReadLine());
-            Console.WriteLine("WHAT'S THE HOURLY RATE?");
-            int hourRate = int.Parse(Console.ReadLine());
-            int wage = 0;
+                Console.WriteLine("NAME : ");
+                string empName = Console.ReadLine();
+                Console.WriteLine("NUMBER OF HOURS :");
+                int hours = int.Parse(Console.ReadLine());
+                Console.WriteLine("WHAT'S THE HOURLY RATE?");
+                int hourRate = int.Parse(Console.ReadLine());
+                int wage = 1;
+                Employee.Wage = wage;
 
-            Employee EMP = new Employee(empName,hours,hourRate, wage);
-              //myEmployees.Add(new Employee(empName,hours,hourRate, wage));
+                
+                myEmployees.Add(new Employee(empName, hours, hourRate));
 
                 Console.WriteLine("ANY MORE EMPLOYEES?");
-            answer = char.ToUpper(char.Parse( Console.ReadLine()));
+                answer = char.ToUpper(char.Parse(Console.ReadLine()));
+                numOf++;
             }
-            
-            
+
+            for (int i = 0; i < numOf; i++)
+            {
+               // Console.WriteLine(myEmployees[i]); it's better to do this insted but that's the excersise
+
+                Console.WriteLine(myEmployees[i].Resultttt());
+            }
+
+
 
             Console.ReadKey();
         }
-        
+
 
     }
 }
